f8e890216aa419661e545a42c85fb4c367eeba43
Maheen <maheensohail2003@gmail.com>
2021-10-06 13:19:29 +0500

    Changing RampUsersFile

--- src/test/scala/simulations/RampUsersSimulation.scala
+++ src/test/scala/simulations/RampUsersSimulation.scala
@@ -3,10 +3,31 @@ package simulations
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import scala.concurrent.duration._
+import scala.util.Random
+
 class RampUsersSimulation extends Simulation {
 
   val httpConf = http.baseUrl("http://localhost:8080/app/")
     .header("Accept", "application/json")
+
+  def userCount: Int = getProperty("USERS", "3").toInt
+  def rampDuration: Int = getProperty("RAMP_DURATION", "10").toInt
+  def testDuration: Int = getProperty("DURATION", "60").toInt
+
+  private def getProperty(propertyName: String, defaultValue: String) = {
+    Option(System.getenv(propertyName))
+      .orElse(Option(System.getProperty(propertyName)))
+      .getOrElse(defaultValue)
+  }
+
+  before {
+    println(s"Running test with ${userCount} users")
+    println(s"Ramping users over ${rampDuration} seconds")
+    println(s"Total Test duration: ${testDuration} seconds")
+  }
 
 
   def GetAllVideoGames() ={
